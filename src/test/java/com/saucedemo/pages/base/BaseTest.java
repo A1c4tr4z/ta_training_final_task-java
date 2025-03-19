dcfa6ea558aac04e1e25372072584dc7c34ecfe1
mrQBS2 <bkadyrzhanov@mtcom.kz>
2025-03-19 08:56:46 +0500

    added order

--- src/test/java/com/saucedemo/pages/base/BaseTest.java
+++ src/test/java/com/saucedemo/pages/base/BaseTest.java
@@ -2,22 +2,29 @@ package com.saucedemo.pages.base;
 
 import com.saucedemo.pages.BasePage;
 import com.saucedemo.pages.LoginPage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.Duration;
 
 
 public class BaseTest {
     protected static WebDriver driver;
     protected static BasePage basePage;
     protected static LoginPage loginPage;
-    private static final String baseUrl = "https://www.saucedemo.com";
+    private static final String baseUrl = "https://www.saucedemo.com/";
 
     private static final Logger logger = LoggerFactory.getLogger(BaseTest.class);
+
+    private static int isFinished = 1;
     @BeforeAll
     static void setUp() {
         logger.info("Launching browser Chrome");
@@ -35,7 +42,22 @@ public class BaseTest {
     static void tearDown() {
         if (driver != null) {
             logger.info("Closing browser");
-            driver.quit();
+        //    driver.quit();
         }
+    }
+
+    @BeforeEach
+    void beforeEachTest() {
+        if (isFinished == 0) {
+            throw new IllegalStateException("Previous test did not finish correctly!");
+        }
+        isFinished=0;
+        logger.info("Starting a new test. isFinished set to 0");
+    }
+
+    @AfterEach
+    void afterEachTest() {
+        isFinished=1;
+        logger.info("Test finished. isFinished set to 1");
     }
 }
--- src/test/java/com/saucedemo/pages/test/LoginTests.java
+++ src/test/java/com/saucedemo/pages/test/LoginTests.java
@@ -3,12 +3,14 @@ package com.saucedemo.pages.test;
 import com.saucedemo.pages.ProductsPage;
 import com.saucedemo.pages.base.BaseTest;
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class LoginTests extends BaseTest {
     @Test
+    @Order(1)
     void testUserNameErrorMessage(){
         loginPage.setUsernameField("");
         loginPage.setPasswordField("");
@@ -18,6 +20,7 @@ public class LoginTests extends BaseTest {
         assertThat("Error does not match the expectation!", errorMessage, Matchers.containsString("Username is required"));
     }
     @Test
+    @Order(2)
     void testPasswordErrorMessage(){
         loginPage.setUsernameField("standard_user");
         loginPage.setPasswordField("");
@@ -27,6 +30,7 @@ public class LoginTests extends BaseTest {
         assertThat("Error does not match the expectation!", errorMessage, Matchers.containsString("Password is required"));
     }
     @Test
+    @Order(3)
     void testLoginSuccess(){
         ProductsPage productsPage = loginPage.login("standard_user", "secret_sauce");
         //Assertions.assertTrue(productsPage.isProductsPagheHeaderDisplayed());
